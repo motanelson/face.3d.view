6865ac76624f04954a893e50aea70c1adfc974ea
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-05-03 14:12:48 +0100

    Delete download.sh

--- download.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/sh 
-printf "\033c\033[43;30m\ndownloa gcc"
-wget "http://www.netgull.com/gcc/releases/gcc-4.0.0/gcc-4.0.0.tar.gz" 
-gzip -d 'gcc-4.0.0.tar.gz'
-tar --extract -f 'gcc-4.0.0.tar'
-cd gcc-4.0.0
